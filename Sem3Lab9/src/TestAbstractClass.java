7fee82900ec33acc8c5c485fa50e4c7479795ba8
Ovidijus38 <55948461+Ovidijus38@users.noreply.github.com>
2020-10-26 15:38:14 +0000

    Lab9

--- Sem3Lab9/src/TestAbstractClass.java
+++ Sem3Lab9/src/TestAbstractClass.java
@@ -13,7 +13,15 @@ public class TestAbstractClass {
         Rectangle rectangle2 = new Rectangle(4.3,1.8);
         System.out.println("\n\nDetails of first Rectangle object: \n" + rectangle1);
 
-        Shape[] allShapes = {circle1,circle2,rectangle1,rectangle2};
+        Triangle triangle1 = new Triangle();
+
+        Triangle triangle2 = new Triangle(3.24,4.23,5.78);
+
+        Ellipse ellipse1 = new Ellipse();
+
+        Ellipse ellipse2 = new Ellipse(5.62,4.19);
+
+        Shape[] allShapes = {circle1,circle2,rectangle1,rectangle2,triangle1,triangle2,ellipse1,ellipse2};
 
         System.out.println("\n\nDetails of all Shape objects contained in the Shape array: \n");
         for(int i=0; i<allShapes.length; i++)
